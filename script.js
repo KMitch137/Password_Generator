03c0ccbd0baa244e36383d35ef1f2277ee9e45fe
kevin mitchell <kmitch137@gmail.com>
2023-05-01 16:12:06 -0700

    changed whole js file after realizing I had it all wrong. started over.

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lower = "abcdefghijklmnopqrstuvwxyz"
-var symbols = "!@#$%^&*()-=+<>?/|':[]{}"
-var nums = "0123456789"
-password = ""
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const symbols = "!@#$%^&*()-=+<>?/|':[]{}";
+const numbers = "0123456789";
+var passWord = "";
+
+function generatePassword() {
+    console.log("button clicked")
+    var passLength = prompt("How many characters would you like you password to use?");
+    if (passLength < 8 || passLength > 128) {
+      alert("Must be between 8 and 128 characters in length. Please try again");
+      return;
+    }
+    var upperCase = confirm("Would you like to use uppercase? 'ok' for yes");
+    var lowerCase = confirm("How about we add some lowercase in there? 'ok' for yes");
+    var symbols = confirm("Lets get crazy with the special characters shall we? 'ok' for yes");
+    var numbers = confirm("Finally, lets add some numbers? 'ok' for yes");
+
+    if (upperCase === true) { passWord += upperCase };
+    if (lowerCase === true) { passWord += lowerCase };
+    if (numbers === true) { passWord += numbers };
+    if (symbols === true) { passWord += symbols };   
+/*
+2. validate the input.
+3. display the pasword on the page.
+*/
+
+    return passWord;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  function generatePassword() {
-    var passLength = prompt("Password must be between 8 and 128 characters!");
-    if (passLength < 8 || passLength > 128) {
-      alert("Must be between 8 and 128 characters in length. Please try again");
-      return;
-    }
-    var upperCase = confirm("Would you like to use uppercase? 'ok' for yes and 'cancel' for no");
-    var lowerCase = confirm("How about we add some lowercase in there? 'ok' for yes and 'cancel' for no");
-    var symbols = confirm("Lets get crazy with the special characters shall we? 'ok' for yes and 'cancel' for no");
-    var numbers = confirm("Finally, lets add some numbers? 'ok' for yes and 'cancel' for no");
 
-    if (upperCase == true) { passwordText += upperCase }
-    if (lowerCase == true) { passwordText += upperCase }
-    if (numbers == true) { passwordText += nums }
-    if (symbols == true) { passwordText += symbols }
-  }
-}
-function generatePassword() {
   passwordText.value = password;
-}
 
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
